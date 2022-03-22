b3f401f4e58b18a4c6192b6b6a78e65509b294f3
andr3slelouch <luis.andradec14@gmail.com>
2022-03-21 22:01:55 -0500

    Updated docstrings for methods, updated requirements.txt

--- testTelegramBot/connect_to_telegram.py
+++ testTelegramBot/connect_to_telegram.py
@@ -11,6 +11,13 @@ import yaml
 
 
 def handler_generate_link_command(update: Update, context: CallbackContext) -> None:
+    """
+    Handler para el comando /link, que generará un link para un grupo y se lo enviará al usuario,
+    por el momento requiere de más estudio y personalización, respecto a los grupos que se genera y la validación.
+    :param update: Update de la librería.
+    :param context:Contexto de la librería.
+    :return:
+    """
     with open("token.yml") as token_f:
         service = yaml.safe_load(token_f)
         group_id = service["group_id"]
@@ -19,18 +26,38 @@ def handler_generate_link_command(update: Update, context: CallbackContext) -> N
 
 
 def hello(update: Update, context: CallbackContext) -> None:
+    """
+    Handler que se encarga de manejar el comando hello util para pruebas por el momento
+    :param update: Update de la librería.
+    :param context:Contexto de la librería.
+    :return: None
+    """
     print(update.message.chat.id)
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
 def handler_new_member_joined(update: Update, context: CallbackContext) -> None:
+    """
+        Handler que se encarga de escuchar cuando un usuario se une al chat y comprueba si este esta permitido
+        PD: Es necesario setearlo con Filters.status_update.new_chat_members para que escuche al usuario que se une
+        :param update: Update de la librería.
+        :param context:Contexto de la librería.
+        :return: None
+        """
     with open("token.yml") as n_token:
         n_service = yaml.safe_load(n_token)
         enabled_users = n_service["enabled_users"]
     print(check_new_chat_member_joined(enabled_users, update.message.new_chat_members))
 
 
-def check_new_chat_member_joined(enabled_users:[str], joined_member_id:str) -> bool:
+def check_new_chat_member_joined(enabled_users: [str], joined_member_id: str) -> bool:
+    """
+    Verifica si un id de usuario pertenece a los usuarios permitidos.
+    :param enabled_users: Lista de usuarios permitidos que debería ser creado luego de que el proceso de
+                            solicitud se ha validado.
+    :param joined_member_id: ID del nuevo miembro que se ha unido al grupo y que debería pertenecer a la lista.
+    :return: True si el usuario pertenece a la lista.
+    """
     for enabled_user in enabled_users:
         if joined_member_id == enabled_user :
             return True
@@ -38,6 +65,13 @@ def check_new_chat_member_joined(enabled_users:[str], joined_member_id:str) -> b
 
 
 def create_one_user_invite_link(update: Update, context: CallbackContext, group_id:str) -> str:
+    """
+    Crea un link para un solo usuario para un grupo específico basado en el id del grupo.
+    :param update: Update de la librería.
+    :param context: Contexto de la librería.
+    :param group_id: ID del grupo del cual se requiere generar un link de invitación.
+    :return: El link de la invitación al grupo especificado a través de su group_id
+    """
     return update.message.bot.create_chat_invite_link(group_id, member_limit=1)
 
 
@@ -46,6 +80,7 @@ def create_main_invite_link(update: Update, context: CallbackContext) -> str:
     Este método debería ejecutarse por primera vez en cada grupo del cual
     se requiera generar links, por el momento depende de que el usuario previamente
     envie un mensaje desde el grupo
+    :return: El link principal de invitación al grupo requerido para poder crear los otros tipos de links
     """
     return update.message.chat.export_invite_link()
 
@@ -59,7 +94,7 @@ updater = Updater(token_str)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('link', handler_generate_link_command))
-updater.dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members,handler_new_member_joined))
+updater.dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, handler_new_member_joined))
 
 updater.start_polling()
 updater.idle()
